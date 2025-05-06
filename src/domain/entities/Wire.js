2fafc43596fe15cce4efadb62bc54892a6aa681b
jurra <jose.urra86@gmail.com>
2025-05-06 12:02:13 +0200

    wire fails to run if no properties are passed.

--- src/domain/entities/Wire.js
+++ src/domain/entities/Wire.js
@@ -1,4 +1,5 @@
 import { Element } from './Element.js';
+import { Properties } from '../valueObjects/Properties.js';
 
 export class Wire extends Element {
     constructor(id, nodes, label = null, properties = new Properties()) {
